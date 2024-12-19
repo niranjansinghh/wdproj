2a1c8821d289e6cb4f4e6e6fe9cc261bc67390a4
Niranjan <160809306+niranjansinghh@users.noreply.github.com>
2024-12-20 02:44:56 +0530

    Update mutations.js

--- Frontend/src/components/mutations.js
+++ Frontend/src/components/mutations.js
@@ -1,13 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const SIGNUP_MUTATION = gql`
-    mutation Signup($email: String!, $password: String!) {
-        signup(email: $email, password: $password) {
+    mutation Mutation($email: String!, $password: String!, $username: String!) {
+        register(email: $email, password: $password, username: $username) {
             token
-            user {
-                id
-                email
-            }
+            
         }
     }
 `; 
+export const LOGIN_MUTATION = gql`
+    mutation Mutation($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+        }
+    }
+`;
